6b5f816662bb1b9fc85d49efec1aadf6f0d9feea
Pavitra Golchha <hello@pavi2410.me>
2025-04-05 19:00:57 +0530

    dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {Toaster} from "@/components/ui/toaster.tsx";
+import { ThemeProvider } from "@/components/theme-provider";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 import { RouterProvider, createRouter } from '@tanstack/react-router'
@@ -23,8 +24,10 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <Toaster />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
